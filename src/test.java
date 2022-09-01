baa91029a1d12c53b972515c1fd39e9e636f60fc
Marcus <marcus1sp2@gmail.com>
2022-09-01 10:16:31 +0200

    🔴 oneToI

--- src/test.java
+++ src/test.java
@@ -1,6 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class test {
 
 
+    @Test
+    void oneToI () {
+        assertTrue(convertToRoman(1) == "I");
+    }
+
+    private String convertToRoman(int i) {
+        return "";
+    }
 
 }
 
